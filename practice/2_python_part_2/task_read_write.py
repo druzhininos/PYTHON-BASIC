797e23178cc083dc5751665d6e59c142ef9baa36
odruzhinin <odruzhinin>
2022-08-26 17:54:00 +0200

    Task task_read_write resolved.

--- practice/2_python_part_2/task_read_write.py
+++ practice/2_python_part_2/task_read_write.py
@@ -13,3 +13,34 @@ Example:
 
     result.txt(content: "23, 78, 3")
 """
+
+contents = []
+
+def read_file(filename):
+    f = open(filename, 'r')
+    contents.append(f.read())
+    f.close()
+    return contents
+
+import os
+path = './files'
+files = os.listdir(path)
+
+for file in files:
+    if os.path.isfile(os.path.join(path, file)):
+        read_file(os.path.join(path, file))
+
+print(contents)
+
+def write_file(filename, data):
+    f = open(filename, 'w')
+    for word in data:
+        print(word, file = f, end = ',')
+    if f.tell():
+        f.seek(f.tell() - 1, 0)
+        f.truncate()
+    f.close()
+
+write_file('result.txt', contents)
+
+print("Task finished.")
