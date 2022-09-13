6f1f264eaf8650867918bea8aff6f0a9e88b8080
odruzhinin <odruzhinin>
2022-09-14 00:23:12 +0200

    Tests for Classes, Parametrize

--- practice/3_python_testing/test_task_classes.py
+++ practice/3_python_testing/test_task_classes.py
@@ -3,3 +3,54 @@ Write tests for classes in 2_python_part_2/task_classes.py (Homework, Teacher, S
 Check if all methods working correctly.
 Also check corner-cases, for example if homework number of days is negative.
 """
+
+from datetime import datetime
+import sys, os
+import pytest
+import datetime
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/2_python_part_2")
+
+# print(sys.path)
+from task_classes import Teacher, Homework, Student
+
+
+@pytest.fixture
+def teacher(scope = "session"):
+    yield Teacher('TLast', 'TFirst')
+
+@pytest.fixture
+def student(scope = "session"):
+    yield Student('SFirst', 'SLast')
+
+@pytest.fixture
+def homework_day(scope = "session"):
+    yield Homework("Homework", 1)
+
+def test_teacher_init(teacher):
+    t = teacher
+    assert t.last_name == 'TLast'
+    assert t.first_name == 'TFirst'
+
+def test_student_init(student):
+    s = student
+    assert s.last_name == 'SLast'
+    assert s.first_name == 'SFirst'
+
+def test_create_homework(teacher):
+    h = teacher.create_homework('Simple homework 1', 1)
+    assert h.text == 'Simple homework 1'
+    assert h.deadline == datetime.timedelta(days = 1)
+
+def test_do_homework(student, homework_day):
+    s = student
+    h = homework_day
+    result = s.do_homework(h)
+    assert result.is_active() == False
+    assert result is not None
+
+def test_do_late_homework(student, homework_day):
+    h = homework_day
+    h.deadline = datetime.timedelta(days = -1)
+
+    result = student.do_homework(h)
+    assert result is None
--- practice/3_python_testing/test_task_parametrize.py
+++ practice/3_python_testing/test_task_parametrize.py
@@ -10,16 +10,30 @@ Task:
  2. Find the buggy function and fix it.
 """
 
+import pytest
 
 def fibonacci_1(n):
     a, b = 0, 1
-    for _ in range(n-1):
-        a, b = b, a + b
-    return b
+    if n > 0:
+        for _ in range(n-1):
+            a, b = b, a + b
+        return b
+    return 0
 
 
 def fibonacci_2(n):
     fibo = [0, 1]
-    for i in range(1, n+1):
+    for i in range(2, n+1):
         fibo.append(fibo[i-1] + fibo[i-2])
     return fibo[n]
+
+fibo_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+fibo_tuples = list(enumerate(fibo_array))
+print (fibo_tuples)
+@pytest.mark.parametrize("n, fibo", fibo_tuples)
+def test_fibonacci_1(n, fibo):
+    assert fibo == fibonacci_1(n)
+
+@pytest.mark.parametrize("n, fibo", fibo_tuples)
+def test_fibonacci_2(n, fibo):
+    assert fibo == fibonacci_2(n)
